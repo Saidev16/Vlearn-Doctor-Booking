8ebe46aa624e277dab755c57f3ec87afd96bfced
saidbtd16 <boutouditsaid@gmail.com>
2021-12-07 15:09:54 +0100

    clientBooking2

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Appointments;
+use App\Entity\Times;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +19,7 @@ class PatientController extends AbstractController
     public function search( Request $request ): Response
     {
         if ($request->isMethod('post')) {
-           return  dd( $request->request->get('search_date'));
+            return $this->redirectToRoute('booking', ['date'=>$request->request->get('search_date')]) ;
         }
         
         return $this->render('patient/index.html.twig', [
@@ -25,12 +29,40 @@ class PatientController extends AbstractController
 
 
     /**
-     * @Route("/booking", name="booking")
+     * @Route("/booking/{date}", name="booking")
      */
-    public function booking( Request $request ): Response
+    public function booking( Request $request , $date): Response
     {
+        
+        $appointmentRepo = $this->getDoctrine()->getRepository(Appointments::class);
+        $appointment = $appointmentRepo->findOneBy(['date'=>$date]);
+
+
+        $timesRepo = $this->getDoctrine()->getRepository(Times::class);
+        $times = $timesRepo->findBy(['appointment_id'=>$appointment->getId() , 'status'=>0]);
+        
+        $usersRepo = $this->getDoctrine()->getRepository(User::class);
+        $doctor = $usersRepo->findOneBy(['id'=>$appointment->getUserId() ]);
+
+        
         return $this->render('patient/book.html.twig', [
-            'controller_name' => 'PatientController',
+            'appointment' => $appointment,
+            'times' => $times,
+            'doctor' => $doctor,
+            'date'=> $date
         ]);
+    }
+
+    /**
+     * @Route("/storeBooking", name="store_booking")
+     */
+    public function storeBooking( Request $request ): Response
+    {
+        $time = $request->request->get('time');
+        $date = $request->request->get('date');
+        $appointmentId = $request->request->get('appointmentId');
+        $doctorId = $request->request->get('doctorId');
+        return dd( $time , $date , $appointmentId , $doctorId ) ;
+
     }
 }
---
+++ src/Entity/Booking.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookingRepository::class)
+ */
+class Booking
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $doctor_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getDoctorId(): ?int
+    {
+        return $this->doctor_id;
+    }
+
+    public function setDoctorId(int $doctor_id): self
+    {
+        $this->doctor_id = $doctor_id;
+
+        return $this;
+    }
+
+    public function getTime(): ?string
+    {
+        return $this->time;
+    }
+
+    public function setTime(string $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
